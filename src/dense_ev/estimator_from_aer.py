46a23ba1bf4b2d640de681a280e40991a991507c
Andrew Lytle <atlytle@gmail.com>
2024-05-22 11:26:06 -0500

    Add copyright info

--- src/dense_ev/estimator_from_aer.py
+++ src/dense_ev/estimator_from_aer.py
@@ -9,6 +9,9 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+#
+# Modifications copyright (C) 2023 University of Illinois at Urbana-Champaign
+# Author: Andrew Lytle
 
 """
 Estimator class.
